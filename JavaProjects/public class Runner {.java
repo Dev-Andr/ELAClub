e9f13f49321dcd0462853f791803f5fa3b1f2825
Aadit Singal <79838616+adi-devv@users.noreply.github.com>
2024-06-15 19:52:37 +0530

    Delete JavaProjects directory

--- JavaProjects/public class Runner {.java
+++
@@ -1,9 +0,0 @@
-public class Runner {
-    public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-        list.insert(5);
-        list.insert(18);
-        list.insert(25);
-        list.show();
-    }    
-}
--- JavaProjects/test3.java
+++
@@ -1,9 +0,0 @@
-public class test3 extends test2 {
-    public void happy(int a) {
-        System.out.println("huihiuihiu");
-    }
-    public static void main(String args[]) {
-        test3 obj = new test3();
-        obj.happy(4);
-    }
-}
