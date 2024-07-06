b27dd34ce8a8422e446da631254b323de1c555bb
Aadit Singal <79838616+adi-devv@users.noreply.github.com>
2024-07-06 20:52:46 +0530

    Update test3.java

--- test3.java
+++ test3.java
@@ -2,6 +2,7 @@ public class test3 extends test2 {
     public void happy(int a) {
         System.out.println("huihiuihiu");
     }
+    
     public static void main(String args[]) {
         test3 obj = new test3();
         obj.happy(4);
